3363c38581e78ccf964e6e6b1fcb936d8c61eaca
David Wilhelm <david.wilhelm@gmx.de>
2018-11-08 15:05:50 +0100

    add lookup-service

---
+++ src/main/java/UserLookupService.java
@@ -0,0 +1,22 @@
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+public class UserLookupService {
+
+    private final RestTemplate restTemplate;
+
+    public UserLookupService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Async
+    CompletableFuture<User> findUser(long id) throws InterruptedException {
+        String url = String.format("https://jsonplaceholder.typicode.com/users/%d", id);
+        User results = restTemplate.getForObject(url, User.class);
+        return CompletableFuture.completedFuture(results);
+    }
+
+}
