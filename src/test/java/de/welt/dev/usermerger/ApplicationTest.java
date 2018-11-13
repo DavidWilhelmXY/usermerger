2257b8343388bf78c1094c954f56916775b70d1c
David Wilhelm <david.wilhelm@gmx.de>
2018-11-13 12:02:51 +0100

    add a few tests

---
+++ src/test/java/de/welt/dev/usermerger/ApplicationTest.java
@@ -0,0 +1,32 @@
+package de.welt.dev.usermerger;
+
+import de.welt.dev.usermerger.data.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        User user = this.restTemplate.getForObject("http://localhost:" + port + "/users/1", User.class);
+        assert user != null;
+        assert user.getId() != null;
+        assert user.getComments() != null;
+        assert user.getComments().length > 0;
+    }
+
+}
